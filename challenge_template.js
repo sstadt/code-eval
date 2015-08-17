684842cceed3ec91c7859f884610a07d475ef70f
Scott Stadt <--global>
2015-08-17 15:00:31 -0500

    initial commit

---
+++ challenge_template.js
@@ -0,0 +1,14 @@
+
+(function () {
+    "use strict";
+
+    var fs = [
+        'test line 1',
+        'test line 3',
+        'test line 3'
+    ];
+
+    fs.forEach(function (line) {
+        console.log(line);
+    });
+}());
---
+++ details/challenge.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    var fs = [
+        'test line 1',
+        'test line 3',
+        'test line 3'
+    ];
+
+    fs.forEach(function (line) {
+        console.log(line);
+    });
+}());
