3833bf8d704d1f5e38223c443370d48a3775e527
scott.stadt@lunch2.com <scott.stadt@lynch2.com>
2016-05-31 14:30:09 -0500

    broken LCD partially accepted

--- medium/broken-lcd/challenge.js
+++ medium/broken-lcd/challenge.js
@@ -3,7 +3,7 @@
     "use strict";
 
     /*
-        You have a 12-digit LCD, each digit consists of 8 segments: 7 segments to display 
+        You have a 12-digit LCD, each digit consists of 8 segments: 7 segments to display
         numbers and one segment to display a decimal mark:
 
          1
@@ -12,25 +12,25 @@
         5 3
          4 8
 
-        The number in each digit is displayed by turning segments on or off. It can be 
-        represented as a binary 8-bit number, each bit of which is a segment, ordered in 
+        The number in each digit is displayed by turning segments on or off. It can be
+        represented as a binary 8-bit number, each bit of which is a segment, ordered in
         the following binary representation:
 
         |_|    01100111
           |    12345678
 
-        For example, number ‘4.’ (with the decimal mark turned on) corresponds to the 
+        For example, number ‘4.’ (with the decimal mark turned on) corresponds to the
         following binary representation:
 
-        Some segments of the display are damaged and are always turned off. Your task is to 
-        determine whether a given number can be displayed on the damaged LCD. You can start 
+        Some segments of the display are damaged and are always turned off. Your task is to
+        determine whether a given number can be displayed on the damaged LCD. You can start
         displaying the number with the arbitrary digit of the LCD.
 
         INPUT SAMPLE:
 
-        The first argument is a filename. Each line of the file contains binary 8-bit numbers, 
-        which represents the state of the segments, starting from the most left digit, and the 
-        number that you must show on the display. The binary numbers are separated by spaces, 
+        The first argument is a filename. Each line of the file contains binary 8-bit numbers,
+        which represents the state of the segments, starting from the most left digit, and the
+        number that you must show on the display. The binary numbers are separated by spaces,
         the number to display is separated by a semicolon.
 
         For example:
@@ -41,13 +41,13 @@
         11111111 01110111 10111011 11001101 11111011 11101010 11110100 01001101 11011111 11111010 10010110 10111111;66.92
         11111011 10010001 11111011 11111101 10011111 10111110 01111100 11011101 10111001 11111110 11101111 11110110;188.87
 
-        Every binary number represents the state of the segments in one digit. 1 means that a 
-        segment is working and can be turned on or off, 0 means that a segment is damaged and 
+        Every binary number represents the state of the segments in one digit. 1 means that a
+        segment is working and can be turned on or off, 0 means that a segment is damaged and
         is always turned off.
 
         OUTPUT SAMPLE:
 
-        Print to stdout 1 for each test case if the number can be displayed on a given LCD, or 
+        Print to stdout 1 for each test case if the number can be displayed on a given LCD, or
         0 – if the number cannot be displayed. Print out one number in a line.
 
         For example:
@@ -80,7 +80,7 @@
             '7': [1, 2, 3],
             '8': [1, 2, 3, 4, 5, 6, 7],
             '9': [1, 2, 3, 4, 6, 7]
-        }
+        };
     }
 
     Digit.prototype.canDisplayDigit = function (digit) {
@@ -117,30 +117,31 @@
 
     Led.prototype.canDisplayNumber = function (num) {
         var nums = num.match(/[0-9](\.)?/g),
+            ok;
+
+        for (var start = 0, lastStart = this.digits.length - nums.length; start < lastStart; start++) {
             ok = true;
 
-        console.log(this.digits);
-        console.log('  ' + JSON.stringify(nums));
-
-        for (var i = 0, j = nums.length; i < j; i++) {
-            console.log('    checking digit ' + nums[i] + ' against ' + this.digits[i].positions);
-            if (!this.digits[i].canDisplayDigit(nums[i])) {
-                ok = false;
-                break;
+            for (var i = 0, j = nums.length; i < j; i++) {
+                if (!this.digits[start + i].canDisplayDigit(nums[i])) {
+                    ok = false;
+                    break;
+                }
             }
+
+            if (ok) break;
         }
 
         return ok;
-    }
+    };
 
     fs.forEach(function (line) {
-        var params = line.split(';'),
+        var lineWithDecimal = (line.indexOf('.') === -1) ? line + '.' : line,
+            params = lineWithDecimal.split(';'),
             status = params[0],
             displayNum = params[1],
             display = new Led(status);
 
-        console.log('checking display against ' + displayNum);
-        console.log('-------------------------------------------')
-        console.log(display.canDisplayNumber(displayNum));
+        console.log(display.canDisplayNumber(displayNum) ? 1 : 0);
     });
 }());
